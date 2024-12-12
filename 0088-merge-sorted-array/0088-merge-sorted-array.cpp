7b605cf03497ea6197256d19f7e956c67f863577
MUHAMMADQASIM6936 <119961355+MUHAMMADQASIM6936@users.noreply.github.com>
2024-12-12 20:23:46 +0500

    Time: 0 ms (100%), Space: 12.5 MB (8.85%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,79 +1,41 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int count = 0;
-        int count1 = 0;
-        int size = m + n;
-        vector<int> arr;
-        vector<int> arr1;
-        int count2 = 0;
 
-        if (m != 0 && n != 0) {
-            for (int i = 0; i < m; i++) {
-                arr.push_back(nums1[i]);
-            }
-            for (int i = 0; i < m + n; i++) {
-                if (count != m && count1 != n) {
-                    if (arr[count] < nums2[count1]) {
-                        arr1.push_back(arr[count]);
-                        count++;
-                    } else if (arr[count] > nums2[count1]) {
-                        arr1.push_back(nums2[count1]);
-                        count1++;
-                    }
-
-                    else {
-                        arr1.push_back(arr[count]);
-                        arr1.push_back(nums2[count1]);
-                        count++;
-                        count1++;
-                    }
-                } else if (count == m || count1 == m) {
-                    for (int i = count; i < m; i++) {
-                        arr1.push_back(arr[i]);
-                    }
-                    for (int i = count1; i < m; i++) {
-                        arr1.push_back(nums2[i]);
-                    }
-                }
-            }
-
-            for (int i = 0; i < m + n; i++) {
-                nums1[i] = arr1[i];
-            }
-        } else if (m == 0) {
-            for (int i = 0; i < n; i++) {
-                nums1[i] = nums2[i];
-            }
-        } else if (m == n) {
-            for (int i = 0; i < m; i++) {
-                arr.push_back(nums1[i]);
-            }
-            for (int i = 0; i < m + n; i++) {
-                if (count != m && count1 != n) {
-                    if (arr[count] < nums2[count1]) {
-                        arr1.push_back(arr[count]);
-                        count++;
-                    } else if (arr[count] > nums2[count1]) {
-                        arr1.push_back(nums2[count1]);
-                        count1++;
-                    }
-
-                    else {
-                        arr1.push_back(arr[count]);
-                        arr1.push_back(nums2[count1]);
-                        count++;
-                        count1++;
-                    }
-                } else if (count == m || count1 == m) {
-                    for (int i = count; i < m; i++) {
-                        arr1.push_back(arr[i]);
-                    }
-                    for (int i = count1; i < m; i++) {
-                        arr1.push_back(nums2[i]);
-                    }
-                }
-            }
+     int count=0;
+     int count1=0;
+     vector<int>arr;
+     while(count<m && count1 < n){
+         if(nums1[count]<nums2[count1]){
+            arr.push_back(nums1[count]);
+            count++;
+         }
+         else if(nums1[count]>nums2[count1]){
+            arr.push_back(nums2[count1]);
+            count1++;
+         }
+         else{
+            arr.push_back(nums1[count]);
+            arr.push_back(nums2[count1]);
+            count++;
+            count1++;
+         }
+     }   
+     if(count==m){
+        while(count1<n){
+            arr.push_back(nums2[count1]);
+            count1++;
         }
+     }
+     else if(count1==n){
+         while(count<m){
+            arr.push_back(nums1[count]);
+            count++;
+        }
+     }
+    for(int i=0;i<m+n;i++){
+        nums1[i]=arr[i];
     }
+    }
+
 };

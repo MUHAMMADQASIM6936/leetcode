c1c7d2983297956716cb1c0f4e13251284bd5f97
MUHAMMADQASIM6936 <119961355+MUHAMMADQASIM6936@users.noreply.github.com>
2025-02-09 22:56:26 +0500

    Time: 0 ms (100%), Space: 19.3 MB (86.73%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,30 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // Create a dummy node to help easily build the merged list
-        ListNode dummy;
-        ListNode* temp3 = &dummy;
 
-        // Traverse both lists and merge them
+        if (list1 == nullptr) return list2;
+        if (list2 == nullptr) return list1;
+
+        ListNode* list3 = nullptr;
+        ListNode* list4 = nullptr;
+
+        // Initialize list3 with the first node (smallest value)
+        if (list1->val < list2->val) {
+            list3 = list1;
+            list1 = list1->next;
+        } else {
+            list3 = list2;
+            list2 = list2->next;
+        }
+
+        list4 = list3; // list4 is the head of the merged list
+
         while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val <= list2->val) {
-                temp3->next = list1;
+            if (list1->val < list2->val) {
+                list3->next = list1;
                 list1 = list1->next;
             } else {
-                temp3->next = list2;
+                list3->next = list2;
                 list2 = list2->next;
             }
-            temp3 = temp3->next;
+            list3 = list3->next; // Move list3 forward
         }
 
-        // If there are remaining nodes in either list, append them
+        // Attach the remaining nodes
         if (list1 != nullptr) {
-            temp3->next = list1;
-        } else if (list2 != nullptr) {
-            temp3->next = list2;
+            list3->next = list1;
+        } else {
+            list3->next = list2;
         }
 
-        // The merged list starts after the dummy node
-        return dummy.next;
+        return list4;
     }
 };

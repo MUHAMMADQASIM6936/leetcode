32737bf2a8aace8186ddf33f3930954ba84d8703
MUHAMMADQASIM6936 <119961355+MUHAMMADQASIM6936@users.noreply.github.com>
2025-08-27 18:28:13 +0500

    Time: 0 ms (100%), Space: 12.2 MB (95.74%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,41 +1,47 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-
-     int count=0;
-     int count1=0;
-     vector<int>arr;
-     while(count<m && count1 < n){
-         if(nums1[count]<nums2[count1]){
-            arr.push_back(nums1[count]);
-            count++;
-         }
-         else if(nums1[count]>nums2[count1]){
-            arr.push_back(nums2[count1]);
-            count1++;
-         }
-         else{
-            arr.push_back(nums1[count]);
-            arr.push_back(nums2[count1]);
-            count++;
-            count1++;
-         }
-     }   
-     if(count==m){
-        while(count1<n){
-            arr.push_back(nums2[count1]);
-            count1++;
+        vector<int>num;
+        for(int i=0;i<m;i++){
+            num.push_back(nums1[i]);
         }
-     }
-     else if(count1==n){
-         while(count<m){
-            arr.push_back(nums1[count]);
-            count++;
-        }
-     }
-    for(int i=0;i<m+n;i++){
-        nums1[i]=arr[i];
-    }
-    }
+        int start=0;
+        int start1=0;
+        int size=0;
+        while(start<m && start1<n){
+            if(num[start]>nums2[start1]){
+                nums1[size]=nums2[start1];
+                start1++;
+                size++;
+            }
+            else if(num[start]<nums2[start1]){
+                nums1[size]=num[start];
+                start++;
+                size++;
+            }
+            else if(num[start]==nums2[start1]){
+                nums1[size]=num[start];
+                start++;
+                size++;
+                nums1[size]=nums2[start1];
+                start1++;
+                size++;
 
+            }
+        }
+        if(start<m){
+        for(int i=start;i<m;i++){
+            nums1[size]=num[i];
+            size++;
+        }
+
+        }
+        else if(start1<n){
+        for(int j=start1;j<n;j++){
+            nums1[size]=nums2[j];
+            size++;
+        }
+
+        }
+    }
 };
